bee4a989b2899d8c246f3a1a8b61d9295fdae071
ptsCLE <ptscle88@gmail.com>
2020-07-19 16:42:07 -0400

    medical

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,59 +1,64 @@
 // ------- PRELOADER -------//
-$(window).load(function(){
-    $('.preloader').fadeOut("slow"); // set duration in brackets    
+$(window).load(function () {
+  $('.preloader').fadeOut("slow"); // set duration in brackets    
 });
 // ----- GOOGLE MAP ----- //
 var map = '';
 var center;
 
 function initialize() {
-    var mapOptions = {
-      zoom: 16,
-      center: new google.maps.LatLng(13.758468, 100.567481),
-      scrollwheel: false
-    };
-  
-    map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
+  var mapOptions = {
+    zoom: 16,
+    center: new google.maps.LatLng(13.758468, 100.567481),
+    scrollwheel: false
+  };
 
-    google.maps.event.addDomListener(map, 'idle', function() {
-        calculateCenter();
-    });
-  
-    google.maps.event.addDomListener(window, 'resize', function() {
-        map.setCenter(center);
-    });
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  google.maps.event.addDomListener(map, 'idle', function () {
+    calculateCenter();
+  });
+
+  google.maps.event.addDomListener(window, 'resize', function () {
+    map.setCenter(center);
+  });
 }
 
 function calculateCenter() {
   center = map.getCenter();
 }
 
-function loadGoogleMap(){
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
-    document.body.appendChild(script);
+function loadGoogleMap() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
+  document.body.appendChild(script);
 }
 
 /* HTML document is loaded. DOM is ready. 
 -------------------------------------------*/
-$(function(){
+$(function () {
 
-   // --------- HIDE MOBILE MENU AFTER CLIKING ON A LINK ------- //
-    $('.navbar-collapse a').click(function(){
-        $(".navbar-collapse").collapse('hide');
-    });
+  // --------- HIDE MOBILE MENU AFTER CLIKING ON A LINK ------- //
+  $('.navbar-collapse a').click(function () {
+    $(".navbar-collapse").collapse('hide');
+  });
 
   // --------- Promo IMAGE ----- //
   $('#promo a').nivoLightbox({
-        effect: 'fadeScale',
-    });
+    effect: 'fadeScale',
+  });
+
+  $('#medical a').nivoLightbox({
+    effect: 'fadeScale',
+  });
+
 
   // ------- WOW ANIMATED ------ //
   wow = new WOW(
-  {
-    mobile: false
-  });
+    {
+      mobile: false
+    });
   wow.init();
 
   // ------- GOOGLE MAP ----- //
